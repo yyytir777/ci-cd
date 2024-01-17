bd6cb990e5d60cbf467e1dc51ec9e652b1070404
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-01-17 22:16:21 +0900

    init - rootController

---
+++ src/main/java/com/example/demo/RootController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "im healthy";
+    }
+}
